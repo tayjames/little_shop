79eb5d01ac54453186503703a9957a69313240cf
Tay <tay.james.d@gmail.com>
2019-07-09 19:24:24 -0600

    Add validations to Merchant model - name, address, city, state, zip
    Co-Authored-By: S. Mainar <smainar@users.noreply.github.com>

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,12 @@
 class Merchant < ApplicationRecord
   has_many :items
 
+  validates_presence_of :name,
+                        :address,
+                        :city,
+                        :state,
+                        :zip
+
   def count_of_items
     self.items.count
   end
