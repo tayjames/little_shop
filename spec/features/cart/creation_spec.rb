0acc149db559293f5ff4910f70ba4aa3d5d726fc
S. Mainar <43945779+smainar@users.noreply.github.com>
2019-07-11 07:16:51 -0600

    Delete save_and_open_page on line 15

--- spec/features/cart/creation_spec.rb
+++ spec/features/cart/creation_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "Cart Creation", type: :feature do
 
     it "In the Item's Show page, I see a link or button to add this item to my cart" do
       visit "/items"
-      save_and_open_page
+      
       expect(page).to have_link(@hippo.name)
       click_on(@hippo.name)
       expect(current_path).to eq("/items/#{@hippo.id}")
